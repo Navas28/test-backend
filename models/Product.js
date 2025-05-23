1dcb458d0a34be79cac78cfdc1cefa79bc19032d
Navas . M <navasmhd79@gmail.com>
2025-05-23 19:39:06 +0530

    created  product schema and product add and edit routes

--- models/Product.js
+++ models/Product.js
@@ -1,20 +1,21 @@
-const mongoose  = require("mongoose");
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  price: { type: Number, required: true },
-  description: String,
-  availability: {
-    type: String,
-    enum: ["in stock", "out of stock"],
-    default: "in stock",
-  },
-  ram: {
-    type: [String], // array of strings
-    enum: ["4GB", "8GB", "16GB"],
-    required: true,
-  },
+const varientSchema = new mongoose.Schema({
+    ram: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
 });
+
+const productSchema = new mongoose.Schema(
+    {
+        title: { type: String, required: true },
+        variants: [varientSchema],
+        subcategory: { type: String, required: true },
+        description: { type: String },
+        images: [{ type: String }],
+    },
+    { timestamps: true }
+);
 
 const Product = mongoose.model("Product", productSchema);
 module.exports = Product;
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,15 +1,61 @@
-const express = require('express')
-const  Product =  require ('../models/Product')
+const express = require("express");
+const Product = require("../models/Product");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", async (req,res) => {
+router.get("/", async (req, res) => {
     try {
-        const products = await Product.find()
-        res.json(products)
+        const products = await Product.find();
+        res.json(products);
     } catch (error) {
-        res.status(500).json({message: "Server error"})
+        res.status(500).json({ message: "Server error" });
     }
-})
+});
+
+router.get("/:id", async (req, res) => {
+    const productId = req.params.id;
+
+    try {
+        const product = await Product.findById(productId);
+        if (!product) return res.status(404).json({ error: "Product not fount" });
+        res.json(product);
+    } catch (error) {
+        res.status(500).json({ error: "Server Error" });
+    }
+});
+
+router.post("/add", async (req, res) => {
+    try {
+        const { title, variants, subcategory, description, images } = req.body;
+        const newProduct = new Product({
+            title,
+            variants,
+            subcategory,
+            description,
+            images,
+        });
+        await newProduct.save();
+        res.status(201).json({ message: "Product added", product: newProduct });
+    } catch (error) {
+        res.status(500).json({ error: "Failed to add product" });
+    }
+});
+
+router.put("/:id", async (req, res) => {
+    try {
+        const updateProduct = await Product.findByIdAndUpdate(
+            req.params.id,
+            {
+                title: req.body.title,
+                description: req.body.description,
+                subcategory: req.body.subcategory,
+                images: req.body.images
+            }, {new: true}
+        )
+        res.json(updateProduct);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to update product" });
+    }
+});
 
 module.exports = router;
